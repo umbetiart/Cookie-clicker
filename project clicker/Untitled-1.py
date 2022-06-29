52ea172628e5898d598f064aa5e4fd899556bf8e
umbetiart <71491983+umbetiart@users.noreply.github.com>
2022-06-29 21:44:24 +0600

    Add files via upload

--- project clicker/Untitled-1.py
+++ project clicker/Untitled-1.py
@@ -1,34 +1,44 @@
+from cProfile import label
 from doctest import master
 from email import message
 from logging import root
 from tkinter import *
-from tkinter import messagebox, Tk, Button
+from tkinter import messagebox, Tk, Button, Canvas, Frame, BOTH
 from PIL import ImageTk
 import time
 
 master = Tk()
+
+bg = PhotoImage(file = "C:/Users/1/Desktop/project clicker/L.png")
+label1 = Label(master, image = bg)
+label1.place(x = 0, y = 0)
 
 def uiPrint():
     info()
     print("")
     print(click)
     blankLine()
-
 def info():
     print("Double click purchases need 50 clicks!")
     print("Auto clicker purchases need 75 clicks!")
+    print("Home made cookies")
+    print("Bakery")
+    print("Factory")
+    print("MINESHAFT")
 
 info()
 
 master.title('Cookie clicker announcement')
-master.iconbitmap('D:/Загрузки/cookie.png')
 master.geometry("300x300")
 click = 0
 mult = 1
 dcp1 = 0
 
+our_button = PhotoImage(file="C:/Users/1/Desktop/project clicker/cookie.png")
+our_button = our_button.subsample(2,2)
+
 def blankLine():
-    for i in range(20):
+    for i in range(5):
         print("")
 
 
@@ -42,7 +52,15 @@ def purchaseDoubleClicksCommand():
         mult = mult*2
         click = click - 5
         messagebox.showinfo("DOUBLE CLICKS PURCHASED", "DOUBLE CLICKS PURCHASED")
+        messagebox.color
         blankLine()
+click = 0 
+label1 = Label(master, textvariable = click, font=('Times 14'), width=20, height=5)
+label1.pack() 
+for i in range(6): 
+    time.sleep(1)   
+    master.update_idletasks()
+
 
 
 def purchaseAutoClickerCommand():
@@ -55,6 +73,18 @@ def purchaseAutoClickerCommand():
         messagebox.showinfo("AUTO CLICKER PURCHASED", "AUTO CLICKER PURCHASED")
         while True:
             click = click + 1
+            time.sleep(1)
+
+def purchaseBakeryCommand():
+    global click
+    if click < 100:
+        print("Not enough clicks!")
+        blankLine()
+    elif click >= 100:
+        click = click - 100
+        messagebox.showinfo("BAKERY PURCHASED", "BAKERY PURCHASED")
+        while True:
+            click = click + 10
             time.sleep(1)
 
 
@@ -84,15 +114,18 @@ def buttonCommand():
         8 TIMES THE CLICKS!''')
         mult = mult * 8
 
-mainClickButton = Button(master, text="Click!", command = buttonCommand)
+
+mainClickButton = Button(master, image=our_button, highlightthickness=0, bd=0, command=buttonCommand)
+mainClickButton.place(x = 190, y=0)
 mainClickButton.pack()
 
 purchaseDoubleClickButton = Button(master, text="Purchase Double Clicks", command = purchaseDoubleClicksCommand)
+purchaseDoubleClickButton.place(x=800, y = 100)
 purchaseDoubleClickButton.pack()
 
 purchaseAutoClickerButton = Button(master, text="Purchase Auto Clicker", command = purchaseAutoClickerCommand)
 purchaseAutoClickerButton.pack()
 
-master.title("Clicker! v0.0.6")
-master.geometry("%sx%s+%s+%s" % (600,600,600,600))
+master.title("Cookie clicker")
+master.geometry("800x533")
 mainloop()
